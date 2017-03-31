5901f1f0fef3e2c33ea0cdd28555ef5cab8c73a5
nryaskin <nik.ryaskin@gmail.com>
2017-03-31 21:04:47 +0300

    updated nearly working services

--- persistence/src/main/java/com/nikita/persistence/facade/PhotoFacade.java
+++ persistence/src/main/java/com/nikita/persistence/facade/PhotoFacade.java
@@ -1,9 +1,11 @@
 package com.nikita.persistence.facade;
 
 import com.nikita.persistence.entities.Photo;
+import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+@Stateless
 public class PhotoFacade extends AbstractFacade<Photo> {
 
     @PersistenceContext(unitName="com.nikita_persistance_jar_1.0-SNAPSHOTPU")
--- persistence/src/main/java/com/nikita/persistence/facade/UserFacade.java
+++ persistence/src/main/java/com/nikita/persistence/facade/UserFacade.java
@@ -1,9 +1,11 @@
 package com.nikita.persistence.facade;
 
 import com.nikita.persistence.entities.User;
+import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+@Stateless
 public class UserFacade extends AbstractFacade<User> {
 
     @PersistenceContext(unitName="com.nikita_persistance_jar_1.0-SNAPSHOTPU")
